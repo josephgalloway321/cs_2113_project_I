2b4bdf16bf849f80e8ea972b613b56ec2e1df744
Joseph D. Galloway II <josephgalloway520@gmail.com>
2024-10-31 10:06:22 -0500

    Working on print dungeon method using Room class

--- DungeonGen.java
+++ DungeonGen.java
@@ -1,10 +1,26 @@
 // Help Sauron create dungeons to store captured hobbits
 
-public class DungeonGen {
+import java.util.ArrayList;
 
+public class DungeonGen {
+  private ArrayList<Room> dungeon;
+
+  public DungeonGen() {
+    dungeon = new ArrayList<>(120);
+  }
+
+  public void printDungeon() {
+    //TODO: Print array in 10x10 grid
+    for (Room room : dungeon) {
+      System.out.println(room);
+    }
+  }
 
   public static void main(String[] args) {
-    String smiley = "\uD83D\uDE00";
-    System.out.println(smiley);
+    
+
+
+
+    
   }
 }
--- Room.java
+++ Room.java
@@ -1,3 +1,8 @@
+
+
 public class Room {
+  final String wall = "\uD83E\uDD86";
+  final String opening = "\u2B1C";
+
   
 }
