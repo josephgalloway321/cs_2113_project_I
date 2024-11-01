a57dae0c3e4c6da18081969f83b1ad7b8b36c1b9
Joseph D. Galloway II <josephgalloway520@gmail.com>
2024-11-01 18:53:51 -0500

    Dungeon generates random rooms for all 10 rows

--- DungeonGen.java
+++ DungeonGen.java
@@ -1,34 +1,62 @@
 // Help Sauron create dungeons to store captured hobbits
+/* Algorithm
+ * 1) Create 10 rows in 2D arraylist with 10 random rooms for each row
+ * 2) Go through each row again using the following subalgorithm
+ *   + Starting on the 2nd room, if my current room has walls on all sides, change room to my left to an opening
+ * 3) ...
+ */
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class DungeonGen {
-  private ArrayList<Room> dungeon;
+  private ArrayList<ArrayList<Room>> dungeon;
+  private Random rand = new Random();
+  private int randomValue;
 
   public DungeonGen() {
-    dungeon = new ArrayList<>(120);
+    dungeon = new ArrayList<ArrayList<Room>>(120);
   }
 
-  public void addRoom(Room room) {
-    dungeon.add(room);
+  public void addRow(ArrayList<Room> row) {
+    dungeon.add(row);
+  }
+
+  public void createRows() {
+    for (int i = 0; i < 10; i++) {
+      ArrayList<Room> row = new ArrayList<Room>(12);
+
+      for (int j = 0; j < 10; j++) {
+        randomValue = rand.nextInt(2);
+        if (randomValue == 0) {
+          Room room = new Room("wall");
+          row.add(room);
+        }
+        else {
+          Room room = new Room("opening");
+          row.add(room);
+        }
+      }
+
+      dungeon.add(row);
+    }
   }
 
   public void printDungeon() {
-    //TODO: Print array in 10x10 grid
-    for (Room room : dungeon) {
-      System.out.print(room.toString());
+    for (ArrayList<Room> row : dungeon) {
+      for (Room room : row) {
+        System.out.print(room.toString());
+      }
+      System.out.println();
     }
-    System.out.println();
   }
 
   public static void main(String[] args) {
     DungeonGen dungeon = new DungeonGen();
 
-    Room object1 = new Room("wall");
-    Room object2 = new Room("opening");
-    dungeon.addRoom(object1);
-    dungeon.addRoom(object2);
+    // Generate dungeon
+    dungeon.createRows();
 
-    dungeon.printDungeon();
+    dungeon.printDungeon(); 
   }
 }
