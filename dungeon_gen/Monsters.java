7674744ab47edf8a975867dae1f5a96c5664f288
Joseph D. Galloway II <josephgalloway520@gmail.com>
2024-11-15 07:00:18 -0600

    Created monster class, still need to test

---
+++ dungeon_gen/Monsters.java
@@ -0,0 +1,66 @@
+package dungeon_gen;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
+public class Monsters {
+  private String monsterType;
+  private int health;
+  private String weaponType;
+
+  Random rand = new Random();
+
+  public Monsters() {
+    setMonsterType();
+    setHealth();
+    setWeaponType();
+  }
+
+  private void setMonsterType() {
+    int randValueToChooseMonster = rand.nextInt(42);  // Values between 0 and 42
+    int lineCounter = 0;
+
+    String monstersFilePath = "monsters.txt";
+    try (BufferedReader bf = new BufferedReader(new FileReader(monstersFilePath))) {
+      String readLine = "";
+      while ((readLine = bf.readLine()) != null) {
+        if (lineCounter == randValueToChooseMonster) {
+          this.monsterType = readLine;
+        }
+        lineCounter++;
+      }
+
+      bf.close();
+    }
+    catch (IOException e) {
+      System.out.println("Error loading file: " + e.getMessage());
+    }
+  }
+
+  private void setHealth() {
+    this.health = rand.nextInt(11) + 10;
+  }
+
+  private void setWeaponType() {
+    int randValueToChooseWeapon = rand.nextInt(42);  // Values between 0 and 42
+    int lineCounter = 0;
+
+    String weaponsFilePath = "weapons.txt";
+    try (BufferedReader bf = new BufferedReader(new FileReader(weaponsFilePath))) {
+      String readLine = "";
+      while ((readLine = bf.readLine()) != null) {
+        if (lineCounter == randValueToChooseWeapon) {
+          this.weaponType = readLine;
+        }
+        lineCounter++;
+      }
+
+      bf.close();
+    }
+    catch (IOException e) {
+      System.out.println("Error loading file: " + e.getMessage());
+    }
+  }
+}
